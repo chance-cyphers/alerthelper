7256895935943736aa8d5165edd9d34fb291a588
chance <chance.a.cyphers@gmail.com>
2019-11-01 18:24:46 -0400

    log app mentions

--- lib/alerthelper_api_web/controllers/event_controller.ex
+++ lib/alerthelper_api_web/controllers/event_controller.ex
@@ -1,10 +1,18 @@
 defmodule AlerthelperApiWeb.EventController do
   use AlerthelperApiWeb, :controller
+  require Logger
 
   def index(conn, params) do
     case params do
       %{"challenge" => msg} -> json(conn, msg)
+      %{"event" => %{"type" => "app_mention"} = event} -> handle_app_mention(conn, event)
       _ -> json(conn, %{hello: "world"})
     end
   end
+
+  defp handle_app_mention(conn, event) do
+    send_resp(conn, 200, "")
+    Logger.info("someone mentioned the app: #{event}")
+  end
+
 end
