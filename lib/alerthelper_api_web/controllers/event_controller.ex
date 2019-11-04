b7b7280cad410dd299fc49222ea4748451903ac9
chance <chance.a.cyphers@gmail.com>
2019-11-03 20:14:09 -0500

    fix: grab token from env

--- lib/alerthelper_api_web/controllers/event_controller.ex
+++ lib/alerthelper_api_web/controllers/event_controller.ex
@@ -13,19 +13,19 @@ defmodule AlerthelperApiWeb.EventController do
   defp handle_app_mention(conn) do
     Logger.info("someone mentioned the app")
 
-    token = Application.get_env(:alerthelper_api, :bot_token)
+    token = System.get_env("BOT_TOKEN")
     headers = [
       {"Content-Type", "application/json"},
       {"Authorization", "Bearer #{token}"}
     ]
     request_body = "{\"channel\": \"#so-many-alerts\", \"text\": \"what is my purpose\"}"
 
-    Logger.info("here's a token: #{token}")
+    Logger.info("here's a tokens: #{token}")
 
     HTTPoison.start()
     HTTPoison.post("https://slack.com/api/chat.postMessage", request_body, headers)
     Logger.info("posted a message to slack, maybe")
-    send_resp(conn, 200, "")
+    send_resp(conn, 200, token)
   end
 
 
